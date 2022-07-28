b9f533dd974403858c91992f8dbaf1acfedbb0af
leejaegu96 <worncjfrn@gmail.com>
2022-07-28 14:16:15 +0900

    test_0728

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -1,4 +1,30 @@
 -- bigbang database를 사용하겠다.
-use bigbang;
+USE bigbang;
 
+CREATE TABLE IF NOT EXISTS `bigbang`.`member2` (
+  `seq` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `nickname` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `password` VARCHAR(45) NULL,
+  `gender` VARCHAR(45) NULL,
+  `dob` VARCHAR(45) NULL,
+  `phone` VARCHAR(45) NULL,
+  `profile` VARCHAR(45) NULL,
+  PRIMARY KEY (`seq`))
+ENGINE = InnoDB
+;
 
+-- 전체 컬럼 조회
+SELECT * FROM member2;
+
+-- 컬럼 추가
+ALTER TABLE member2 ADD COLUMN bobby VARCHAR(45);
+
+ALTER TABLE member2 ADD COLUMN nameEng VARCHAR(45) AFTER name;
+
+-- 컬럼 변경(데이터가 없을 때 가능)
+ALTER TABLE member2 MODIFY COLUMN bobby VARCHAR(100);
+
+-- 컬럼 이름 변경
+ALTER TABLE member2 CHANGE COLUMN bobby hobby VARCHAR(100);
