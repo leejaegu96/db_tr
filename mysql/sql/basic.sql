ae028885f0e5dba9491dc85c06565b86eb7ca1a3
leejaegu96 <worncjfrn@gmail.com>
2022-07-28 17:33:59 +0900

    test_0728

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -15,6 +15,30 @@ CREATE TABLE IF NOT EXISTS `bigbang`.`member2` (
 ENGINE = InnoDB
 ;
 
+INSERT INTO member2(
+	seq
+    ,name
+    ,nickname
+    ,email
+    ,password
+    ,gender
+    ,dob
+    ,phone
+    ,profile
+    )
+VALUES (
+	"1"
+	,"Leegana"
+	,"gana"
+    ,"123@naver.com"
+    ,"aaaa"
+    ,"1"
+    ,"19960101"
+    ,"010-1111-1111"
+    ,"a"
+)
+;
+
 -- 전체 컬럼 조회
 SELECT * FROM member2;
 
@@ -28,3 +52,18 @@ ALTER TABLE member2 MODIFY COLUMN bobby VARCHAR(100);
 
 -- 컬럼 이름 변경
 ALTER TABLE member2 CHANGE COLUMN bobby hobby VARCHAR(100);
+
+-- 컬럼 삭제
+ALTER TABLE member2 drop COLUMN hobby;
+
+-- row삭제 DELETE FROM WHERE seq = 1 먼저 쓴 후에 member2 넣기
+DELETE FROM member2 WHERE seq = 1;
+
+-- commit / rollback
+SELECT * FROM member2;
+
+-- 데이터 내용 변경 where 먼저 쓰기.
+UPDATE member2 SET 
+name= "leejaegu" 
+, nickname="jaeng"
+WHERE seq = "1";
